931ebec54d3fea7018ebd7d27fd5597129b45f63
Aleksandr Koloshtivin <aykolosh@mts.ru>
2021-11-27 18:35:10 +0300

    Some improvements

--- src/main/java/CatGallery.java
+++ src/main/java/CatGallery.java
@@ -2,14 +2,12 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.http.HttpHeaders;
 import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.entity.ContentType;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -36,7 +34,7 @@ public class CatGallery {
         getSortedPosts().stream().forEach(System.out::println);
     }
 
-    private void createHttpClient() throws IOException {
+    private void createHttpClient() {
         httpClient = HttpClientBuilder.create()
                 .setDefaultRequestConfig(RequestConfig.custom()
                         .setConnectTimeout(5000)
